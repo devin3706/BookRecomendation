dbed6f52ad69538dc91f4ba9588bdf4550c7df05
Devin Silva <sdevinsilva@gmail.com>
2024-10-04 21:07:34 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 import numpy as np
 
-st.header('Book Recommender System Using Machine Learning')
+st.header('Book Recommender')
 model = pickle.load(open('artifacts/model.pkl', 'rb'))
 book_names = pickle.load(open('artifacts/books_name.pkl', 'rb'))
 final_rating = pickle.load(open('artifacts/final_rating.pkl', 'rb'))
